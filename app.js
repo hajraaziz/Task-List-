383f4798924ab9559e205b9a0d58fc66b55a0d03
hajraaziz <hajraaziz10@gmail.com>
2025-09-20 03:48:32 +0500

    Persist to Local Storage

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load all event listeners
 function loadEventListeners() {
+    // DOM load event 
+    document.addEventListener('DOMContentLoaded', getTasks);
     // Add task event
     form.addEventListener('submit', addTask);
     // remove task event
@@ -20,6 +22,35 @@ function loadEventListeners() {
     filter.addEventListener('keyup', filterTasks);
 }
 
+// Get Task from LS
+function getTasks(e) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task){
+        // Create li element
+        const li = document.createElement('li');
+        // Add class
+        li.className = 'collection-item';
+        // Create textNode and append to li
+        li.appendChild(document.createTextNode(task));
+        // Create new link element
+        const link = document.createElement('a');
+        link.className = 'delete-item secondary-content';
+        // Add icon HTML
+        link.innerHTML = '<i class="fa fa-remove"></i>'
+        // Append the link to li
+        li.appendChild(link);
+
+        // Append li to ul
+        taskList.appendChild(li);
+    }) 
+}
+
 // Add Task function
 function addTask(e) {
     if(taskInput.value === '') {
@@ -28,6 +59,7 @@ function addTask(e) {
 
     // Create li element
     const li = document.createElement('li');
+    // Add class
     li.className = 'collection-item';
     // Create textNode and append to li
     li.appendChild(document.createTextNode(taskInput.value));
@@ -42,10 +74,27 @@ function addTask(e) {
     // Append li to ul
     taskList.appendChild(li);
 
+    // Store in LS
+    storeTaskInLocalStorage(taskInput.value);
+
     // clear Input
     taskInput.value = '';
 
     e.preventDefault();
+}
+
+// Store task function
+function storeTaskInLocalStorage(task) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove task function
@@ -53,10 +102,29 @@ function removeTask(e) {
     if(e.target.parentElement.classList.contains('delete-item')) {
         if(confirm('Are you sure?')) {
             e.target.parentElement.parentElement.remove();
+
+            // remove from LS
+            removeTaskFromLocalStorage(e.target.parentElement.parentElement);
         }
     }
+}
 
-    e.preventDefault();
+// Remove task from LS function
+function removeTaskFromLocalStorage(taskItem) {
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // clear tasks function
@@ -67,7 +135,13 @@ function clearTasks(e) {
         taskList.removeChild(taskList.firstChild);
     }
 
-    e.preventDefault();
+    // clear task from LS
+    clearTasksFromLocalStorage();
+}
+
+// Clear Tasks from LS
+function clearTasksFromLocalStorage(){
+    localStorage.clear();
 }
 
 // filter tasks function
@@ -75,15 +149,13 @@ function filterTasks(e) {
     const text = e.target.value.toLowerCase();
 
     document.querySelectorAll('.collection-item').forEach(
-        function(tasks) {
-            const item = tasks.firstChild.textContent;
+        function(task) {
+            const item = task.firstChild.textContent;
             if(item.toLocaleLowerCase().indexOf(text) != -1) {
-                tasks.style.display = 'block';
+                task.style.display = 'block';
             } else {
-                tasks.style.display = 'none';
+                task.style.display = 'none';
             }
         }
     );
-
-    e.preventDefault();
 }
